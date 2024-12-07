d3a458e3e72b25b353bd24416820e3d6bb8f0d40
Carl Assmann <assmann@hey.com>
2024-12-07 10:12:00 +0100

    add lazygit config, add more keybindings to helix config, use node 22

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,11 @@
 
 # install node version manager and node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 22
+nvm use 22
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -28,7 +32,6 @@ npm install -g \
   vscode-langservers-extracted
 
 # create symlinks
-
 script_dir=$(dirname "$(readlink -f "$0")")
 dotfiles_dir="$script_dir"
 ignore_list=(
