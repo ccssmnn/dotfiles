4120b914715a7a3d1a6e727f70f5781cf3d9d010
Carl Assmann <assmann@hey.com>
2025-03-16 22:33:26 +0100

    sync

--- scripts/bin/ai.js
+++ scripts/bin/ai.js
@@ -11,28 +11,38 @@ import { generateText } from "ai";
 import { google } from "@ai-sdk/google";
 import { removeCodeFence } from "../src/utils.js";
 
-readEnvFromLocalFile();
+let __filename = fileURLToPath(import.meta.url);
+let envPath = resolve(dirname(__filename), "..", ".env");
+dotenv.config({ path: envPath });
+
 let input = await readLinesFromStdin();
 let files = await readFilesFromArgs();
 let aiResponse = await generateText({
   model: google("gemini-2.0-flash"),
   prompt: `
-You are a code assistant that helps developers improve their code. You receive an input that needs to be replaced, and your task is to suggest a replacement.
+## Some rules to follow:
 
-Some rules to follow:
+You are a code assistant that helps developers improve their code.
+
+- write code that explains itself and only add comments where the intention needs explaination
 - prefer let over const for variable declaration
 - prefer function example() {} over const example = () => {}
-- avoid comments in the code when descriptive variable names are sufficient
 
-You will be provided with relevant files from the codebase to provide context. Pay close attention to the file paths and contents to understand the surrounding code. If you find any TODO: comments treat them as instructions and replace them with an "AI: eplaining the reasoning" comment where the developer can see what the reasoning behind the change was.
+## The Task:
 
-Files for context:
-${files.map(({ path, file }) => `File: ${path}\n${file}\n`).join("")}
+You will receive an input that needs to be replaced, and your task is to suggest a replacement.
+You will be provided with relevant files from the codebase to provide context.
+Pay close attention to the file paths and contents to understand the surrounding code.
+If you find any TODO: comments treat them as instructions and replace them with an AI: comment where the developer can see what the reasoning behind the change was.
+In the unlikely case where you don't want to change anything, add an AI: comment explaining why.
 
-Input to be replaced:
+## Files for context:
+${files.map(({ path, file }) => "### File: " + path + "\n" + file + "\n").join("")}
+
+## Input to be replaced:
 ${input}
 
-Final note:
+## Final note:
 It is important that you only respond with text that can directly replace the input, without any surrounding code fences or explanations.
 `,
 });
@@ -70,8 +80,4 @@ async function readFilesFromArgs() {
   return files;
 }
 
-function readEnvFromLocalFile() {
-  let __filename = fileURLToPath(import.meta.url);
-  let envPath = resolve(dirname(__filename), "..", ".env");
-  dotenv.config({ path: envPath });
-}
+function readEnvFromLocalFile() {}
